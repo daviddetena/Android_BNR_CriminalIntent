5303fd4f5342b662d0763fa7d685ce31c35e9bfd
David de Tena <daviddetenacalvo@gmail.com>
2015-10-06 00:55:20 +0200

    Variables for date and solved added in Crime.java. Included their proper widgets in fragment_crime layout too.

--- app/src/main/java/com/daviddetena/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/daviddetena/criminalintent/CrimeFragment.java
@@ -5,9 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment{
@@ -15,8 +19,10 @@ public class CrimeFragment extends Fragment{
     // Model
     private Crime mCrime;
 
-    // View
+    // Views
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckbox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,12 @@ public class CrimeFragment extends Fragment{
 
         // Wire up UI
         mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mSolvedCheckbox = (CheckBox) v.findViewById(R.id.crime_solved);
+
+        // Set Crime's date as button default date
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
 
         // Set listener for the edit text when text changed
         mTitleField.addTextChangedListener(new TextWatcher() {
@@ -52,6 +64,15 @@ public class CrimeFragment extends Fragment{
             }
         });
 
+        // Listener for Checkbox toggling
+        mSolvedCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Set the crime's solved property
+                mCrime.setSolved(isChecked);
+                Log.i("hello", String.format("%b",mCrime.isSolved()));
+            }
+        });
 
         return v;
     }
