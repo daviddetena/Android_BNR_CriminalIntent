0cd9cd14daa94a445ef8d2bd950547f79b619970
David de Tena <daviddetenacalvo@gmail.com>
2015-10-14 01:06:00 +0200

    Communication between CrimeFragment and DataPickerFragment. Now the DatePickerFragment gets current crime's date and display it (data sent via ARGS), and the DatePickerFragment sends the date selected back as an EXTRA in an Intent. CrimeFragment is self-assigned as the TargetFragment when creating a new instance of DatePickerFragment, and DPF sends intent by calling onActivityResult() method of this target fragment.

--- app/src/main/java/com/daviddetena/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/daviddetena/criminalintent/DatePickerFragment.java
@@ -1,14 +1,30 @@
 package com.daviddetena.criminalintent;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DatePickerFragment extends DialogFragment {
+
+    // ARG in Bundle to get data from
+    private static final String ARG_DATE = "date";
+    // EXTRA to put data in an Intent to send back to the target fragment
+    public static final String EXTRA_DATE = "com.daviddetena.android.criminalintent.date";
+
+    // DatePicker to save what user selects
+    private DatePicker mDatePicker;
 
     /**
      * Method from DialogFragment class that is called when putting DialogFragment on screen
@@ -18,13 +34,71 @@ public class DatePickerFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        // Get current crime's Date from the ARGS and create a calendar with it
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         // Set a view with the layout inflated from dialog_date layout and assign to AlertDialog's
-        // view
+        // view. Create a DatePicker with the crime's date and init it
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+        mDatePicker = (DatePicker) view.findViewById(R.id.dialog_date_date_picker);
+        mDatePicker.init(year, month, day, null);
+
         return new AlertDialog.Builder(getActivity())
                 .setView(view)
                 .setTitle(R.string.date_picker_title)
-                .setPositiveButton(android.R.string.ok, null)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int year = mDatePicker.getYear();
+                        int month = mDatePicker.getMonth();
+                        int day = mDatePicker.getDayOfMonth();
+
+                        // Compose a date from the integers of year, month, and day selected
+                        // in the datepicker
+                        Date date = new GregorianCalendar(year, month, day).getTime();
+                        sendResult(Activity.RESULT_OK, date);
+                    }
+                })
                 .create();
+    }
+
+
+    /**
+     * Get a DatePickerFragment object with arguments from a parameter passed in
+     * @param date
+     * @return
+     */
+    public static DatePickerFragment newInstance(Date date){
+        // Create a new Bundle for arguments
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        // Create a new instance of DatePickerFragment and return it
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    /**
+     * Method to send data back as an EXTRA to target fragment
+     * @param resultCode
+     * @param date
+     */
+    private void sendResult(int resultCode, Date date){
+        if(getTargetFragment() == null){
+            return;
+        }
+        // Creates an intent to send data back to target fragment
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATE, date);
+
+        getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, intent);
     }
 }
