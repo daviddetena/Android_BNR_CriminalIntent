3796ed95ad469259ba6f8e131bd847d3a9433005
David de Tena <daviddetenacalvo@gmail.com>
2015-10-15 20:23:28 +0200

    Implemented menu action to add a new Crime objects.

--- app/src/main/java/com/daviddetena/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/daviddetena/criminalintent/CrimeListFragment.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -64,6 +65,28 @@ public class CrimeListFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
         // Populate the Menu instance with the items defined in fragment_crime_list.xml menu
         inflater.inflate(R.menu.fragment_crime_list, menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Process to be done regarding the item selected
+        switch(item.getItemId()){
+
+            // Create new Crime, add it to the list and present this Crime with ViewPager
+            case R.id.menu_item_new_crime:
+                Crime crime = new Crime();
+                CrimeLab.get(getActivity()).addCrime(crime);
+                Intent intent = CrimePagerActivity.newIntent(getActivity(), crime.getId());
+                startActivity(intent);
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     /**
