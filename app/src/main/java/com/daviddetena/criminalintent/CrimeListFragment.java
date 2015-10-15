f006e9cc5634c97715da19ad513f0755e5869781
David de Tena <daviddetenacalvo@gmail.com>
2015-10-15 02:07:28 +0200

    Created an xml resource for the menu with an item. Menu inflated in CrimeListFragment's onCreateOptionsMenu().

--- app/src/main/java/com/daviddetena/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/daviddetena/criminalintent/CrimeListFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -20,6 +22,14 @@ public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Let the hosting Activity's Fragment Manager know that this Fragment needs to receive
+        // menu callbacks
+        setHasOptionsMenu(true);
+    }
 
     @Nullable
     @Override
@@ -47,6 +57,13 @@ public class CrimeListFragment extends Fragment {
     public void onResume(){
         super.onResume();
         updateUI();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        // Populate the Menu instance with the items defined in fragment_crime_list.xml menu
+        inflater.inflate(R.menu.fragment_crime_list, menu);
     }
 
     /**
